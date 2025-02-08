e218c14f20d2b2c4a38a971387f4b1fbe7769b7b
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-02-08 20:07:36 +0530

    added all apis in frontend

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [title, setTitle] = useState("");
   const [wordLen, setWordLen] = useState("short");
   const [summaryType, setSummaryType] = useState("concept");
-
   const [summary, setSummary] = useState(null);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [allQuestionsAndAnswers, setAllQuestionsAndAnswers] = useState([]);
 
   const handleSummary = async () => {
     if (!title.trim()) {
@@ -52,30 +55,130 @@ export default function Home() {
     }
   };
 
+  const handleSimilar = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/suggestNextSummary/${title}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to find similar book name");
+      }
+
+      const data = await response.json();
+      setTitle(data);
+      setSummary("");
+    } catch (error) {
+      console.error(error);
+    }
+    stopSpeech();
+  };
+
+  const handleSearchAgain = () => {
+    stopSpeech();
+    setSummary("");
+    setTitle("");
+  };
+
+  const handleStartSpeech = () => {
+    if (!summary) return;
+
+    const utterance = new SpeechSynthesisUtterance(summary);
+    utterance.onend = () => setIsSpeaking(false);
+
+    speechSynthesis.speak(utterance);
+    setIsSpeaking(true);
+  };
+
+  const handleStopSpeech = () => {
+    speechSynthesis.cancel();
+    setIsSpeaking(false);
+  };
+
+  const handleFindAnswer = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/findAnswer/${title}/${summary}/${question}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to find answer of your question");
+      }
+
+      const data = await response.json();
+      setAnswer(data);
+
+      setAllQuestionsAndAnswers([
+        ...allQuestionsAndAnswers,
+        { question, answer: data },
+      ]);
+      setQuestion("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <input
-        type="text"
-        value={title}
-        placeholder="Enter the book name"
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <select onChange={(e) => setWordLen(e.target.value)} value={wordLen}>
-        <option value="short">Short</option>
-        <option value="medium">Medium</option>
-        <option value="long">Long</option>
-      </select>
-      <select
-        onChange={(e) => setSummaryType(e.target.value)}
-        value={summaryType}
-      >
-        <option value="purpose">Purpose</option>
-        <option value="concept">Concept</option>
-        <option value="plot">Plot</option>
-      </select>
-      <button onClick={handleSummary}>Get Summary</button>
-      <button onClick={handleSuggestion}>Suggest bookname</button>
-      {summary && <p>{summary}</p>}
+      {!summary && (
+        <>
+          <input
+            type="text"
+            value={title}
+            placeholder="Enter the book name"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <select onChange={(e) => setWordLen(e.target.value)} value={wordLen}>
+            <option value="short">Short</option>
+            <option value="medium">Medium</option>
+            <option value="long">Long</option>
+          </select>
+          <select
+            onChange={(e) => setSummaryType(e.target.value)}
+            value={summaryType}
+          >
+            <option value="purpose">Purpose</option>
+            <option value="concept">Concept</option>
+            <option value="plot">Plot</option>
+          </select>
+          <button onClick={handleSummary}>Get Summary</button>
+          <button onClick={handleSuggestion}>Suggest bookname</button>
+        </>
+      )}
+
+      {summary && (
+        <>
+          <p>{summary}</p>
+          <button onClick={handleSimilar}>Find similar</button>
+          <button onClick={handleSearchAgain}>Search Again</button>
+          {isSpeaking ? (
+            <button onClick={handleStopSpeech}>Stop Listening</button>
+          ) : (
+            <button onClick={handleStartSpeech}>Start Listening</button>
+          )}
+          <input
+            type="text"
+            value={question}
+            placeholder="Do you have any question?"
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <button onClick={handleFindAnswer}>Find Answer Quickly</button>
+          {allQuestionsAndAnswers.length > 0 &&
+            allQuestionsAndAnswers.map((ans, index) => (
+              <div key={index}>
+                <h3>{ans.question}</h3>
+                <p>{ans.answer}</p>
+              </div>
+            ))}
+        </>
+      )}
     </>
   );
 }
