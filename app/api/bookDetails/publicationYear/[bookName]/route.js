cfb950c4478a841d7b352bb7d05380efdfde10b8
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-02-09 22:18:57 +0530

    created api for book detail

---
+++ app/api/bookDetails/publicationYear/[bookName]/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+export async function POST(_, { params }) {
+  try {
+    const { bookName } = await params;
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: `Give only publication year of this book: ${bookName}`,
+                },
+              ],
+            },
+          ],
+        }),
+      }
+    );
+    const result = await response.json();
+    return NextResponse.json(result.candidates[0].content.parts[0].text);
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
+}
